2c462d092d8f75c6744afe6b7356f4f28e5a1cdb
mrrichmine <vit_1995@mail.ru>
2016-11-14 18:01:19 +0500

    #2 - CSS fixes, auth button added (no working)

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -2,14 +2,15 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'app-header',
     template:`
-        <header class="row">
-            <nav class="navbar navbar-brand">
-                <ul class="nav">                  
-                    <h2><img alt="МФЦ" src="../img/MD_Sign_Smaller.png" />
-                    Корпоративный портал МФЦ Свердловской Области</h2>
-                </ul>
-            </nav>            
-        </header>
+        <div class="row">  
+            <div class="col-md-10">            
+                <h2><img alt="МФЦ" src="../img/MD_Sign_Smaller.png" />
+                Корпоративный портал МФЦ Свердловской Области</h2>
+            </div>
+            <div class="col-md-2">
+                <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>  Вход на Портал
+            </div>
+        </div>
     `
 })
 export class HeaderComponent {}
